b28fb83bdeb90eb32336bfd34378752e39b62e75
ödutolaabisoye <dutolaabisoye@gmail.com>
2020-06-17 23:53:41 +0100

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,95 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.start,
 
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                  // a container can only have single child
-                  width: 100.0,
-                  height: double.infinity,
-                  padding:
-                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-                  //margin:
-                      //EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-                  //margin: EdgeInsets.fromLTRB(left, top, right, bottom),
-                  //margin: EdgeInsets.only(left: 30px),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/xtreme.jpeg'),
+              ),
+              Text(
+                'Xtreme Spencer',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.00,
                   color: Colors.white,
-                  child: Text('Container `1')),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 24.00,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
               SizedBox(
-                width: 20.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                height: 100.0,
-                child: Text('Container 2'),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        size: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'odutolaabisoye@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Container(
-                color: Colors.red,
-                width: 100.0,
-                height: double.infinity,
-                child: Text('Container 3'),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        size: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+2349098833141',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
@@ -54,3 +108,18 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+//Padding(
+//padding: const EdgeInsets.all(10.0),
+//child: Row(
+//mainAxisAlignment: MainAxisAlignment.center,
+//children: <Widget>[
+//
+//SizedBox(
+//width: 10.0,
+//),
+//
+//],
+//),
+//),
